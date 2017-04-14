3f91b02bebc334a8a6f5e0d4cfd2d53e055e7133
Graham Bing <grrraham@gmail.com>
2017-04-13 20:30:05 -0700

    improved default baked_data.c, otherwise there are errors building lookup before running init

--- baked_data.c
+++ baked_data.c
@@ -1,8 +1,6 @@
 #include "trie.h"
 
-size_t baked_trie[] = { 8, 16384, 268435520, 268435456, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+size_t baked_trie[] = { 0,0,0,0,0,0,0,0,0,0,0 };
 
 const char * baked_vals[] = {
-	"key not found",
-	"furry dumb",
-	"or do not, there is no trie"};
+	"key not found" };
